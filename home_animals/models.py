924fbd06cd18ac55cb9bba9b8a1c4ee13eb1ae87
Evelyn Foy <evelyn.foy@hotmail.com>
2022-05-11 22:45:12 +0100

    Revert "Revert "12 user story manage animal type list""

--- home_animals/models.py
+++ home_animals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class AnimalType(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code} - {self.description}"
