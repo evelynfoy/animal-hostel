ba415368875e322b9f8acf58301e8346295c0cae
evelynfoy <evelyn.foy@hotmail.com>
2022-05-12 18:19:55 +0000

    Add animal model to admin.py

--- home_animals/admin.py
+++ home_animals/admin.py
@@ -1,8 +1,16 @@
+''' Admin '''
 from django.contrib import admin
-from .models import AnimalType 
+from .models import AnimalType, Animal
 
 
 @admin.register(AnimalType)
 class AnimalTypeAdmin(admin.ModelAdmin):
+    ''' Animal Type '''
 
     list_display = ('code', 'description')
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    ''' Animal '''
+    list_display = ('name', 'type', 'slogan', 'description')
