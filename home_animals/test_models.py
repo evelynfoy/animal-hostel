793b16338fce799e1579c9848849fa25e178954d
Evelyn Foy <evelyn.foy@hotmail.com>
2022-05-13 07:07:20 +0100

    Revert "11 user story manage guest list"

--- home_animals/test_models.py
+++ home_animals/test_models.py
@@ -1,22 +1,9 @@
-''' Test Models '''
 from django.test import TestCase
-from .models import AnimalType, Animal
+from .models import AnimalType
 
 
 class TestModels(TestCase):
-    ''' Test Models '''
 
     def test_animal_type_creation(self):
-        ''' Tests creation of Animal Type'''
         animal_type = AnimalType(code='Cat', description='Cat')
-        self.assertEqual(str(animal_type), 'Cat')
-
-    def test_animal_creation(self):
-        '''
-        Tests creation of an Animal
-        '''
-        animal_type = AnimalType(code='Cat', description='Cat')
-        animal = Animal(name='Smokey', slogan='Grey cat', slug='smokey',
-                        type=animal_type,
-                        description='Smokey is a perfect gentleman of a cat.')
-        self.assertEqual(str(animal), 'Smokey')
+        self.assertEqual(str(animal_type), 'Cat - Cat')
