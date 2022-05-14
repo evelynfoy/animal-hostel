180dc4d9cda7e98c8c9df604c8fa0c4bee0fe8a7
evelynfoy <evelyn.foy@hotmail.com>
2022-05-14 11:32:20 +0000

    Add Guest List view to test_views.py

--- home_animals/views.py
+++ home_animals/views.py
@@ -1,3 +1,15 @@
+''' Views '''
 from django.shortcuts import render
+from django.views import View
+from .models import Animal
 
-# Create your views here.
+
+class GuestList(View):
+    ''' Guest List '''
+    template_name = 'index.html'
+
+    def get(self, request):
+        ''' Get view for displaying the Guests '''
+        animal = Animal.objects.order_by('name')
+        return render(request, self.template_name,
+                      {'animal_list': animal})
