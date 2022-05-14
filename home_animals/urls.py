50da9576bd3bddf9ce59f71ff4eda1c286ec5456
evelynfoy <evelyn.foy@hotmail.com>
2022-05-14 18:42:20 +0000

    Add urls.py to app and include in project version

---
+++ home_animals/urls.py
@@ -0,0 +1,11 @@
+''' urls for Home Animal app '''
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.GuestList.as_view(), name='home'),
+    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    # path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
+]
