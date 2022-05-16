366ea709929828d6345d4ef234abc9560f438138
evelynfoy <evelyn.foy@hotmail.com>
2022-05-16 20:16:46 +0000

    Add detail view of guest/animal

--- home_animals/views.py
+++ home_animals/views.py
@@ -1,7 +1,9 @@
 ''' Views '''
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from django.views import View
 from .models import Animal
+# from .forms import GuestDetailForm
 
 
 class GuestList(View):
@@ -13,3 +15,19 @@ class GuestList(View):
         animal = Animal.objects.order_by('name')
         return render(request, self.template_name,
                       {'animal_list': animal})
+
+
+class GuestDetail(View):
+
+    ''' Guest List '''
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Animal.objects
+        guest = get_object_or_404(queryset, slug=slug)
+        return render(
+            request,
+            "guest_detail.html",
+            {
+                "guest": guest,
+            }
+        )
