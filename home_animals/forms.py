8aaf9a6bb5f0d8ed72baf77e343e98d6a28583e1
evelynfoy <evelyn.foy@hotmail.com>
2022-05-26 17:40:05 +0000

    Add different forms for Add, Edit and Deleting Offers

--- home_animals/forms.py
+++ home_animals/forms.py
@@ -1,18 +1,65 @@
-""" Form """
+""" Contains three forms used by the Offer CRUD functionality """
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from .models import Offer
 
 
-class OfferForm(forms.ModelForm):
-    """ Offer Form """
+class OfferCreateForm(forms.ModelForm):
+    """
+        Defines the 'Make an Offer form' which alters the field names
+        displayed to make the form more user friendly
+        Based on Model : Offer
+    """
     class Meta:
         """ Meta """
         model = Offer
         fields = ('animal', 'pitch', 'basis', 'weeks')
         labels = {
-                    'animal': _('Guest'),
-                    'pitch': _('Why I would make a great owner'),
-                    'basis': _('Adoption or Fostering'),
-                    'weeks': _('If fostering please enter number of weeks:')
-                }
+            'animal': _('Guest'),
+            'pitch': _('Why I would make a great owner'),
+            'basis': _('Adoption or Fostering'),
+            'weeks': _('If fostering please enter number of weeks:')
+        }
+
+
+class OfferEditForm(forms.ModelForm):
+    """
+        Defines the 'Edit form' which alters the field names
+        displayed to make the form more user friendly.
+        It does not include the Animal name so it cannot be altered
+        Based on Model : Offer
+    """
+    class Meta:
+        """ Meta """
+        model = Offer
+        fields = ('pitch', 'basis', 'weeks')
+        labels = {
+            'pitch': _('Why I would make a great owner'),
+            'basis': _('Adoption or Fostering'),
+            'weeks': _('If fostering please enter number of weeks:')
+        }
+
+
+class OfferDeleteForm(forms.ModelForm):
+    """
+        Defines the 'Delete Offer form' which alters the field names
+        displayed to make the form more user friendly.
+        It does not include the Animal name so it cannot be altered.
+        It also sets the fields displayed to disabled.
+        Based on Model : Offer
+    """
+    class Meta:
+        """ Meta """
+        model = Offer
+        fields = ('pitch', 'basis', 'weeks')
+        labels = {
+            'pitch': _('Why I would make a great owner'),
+            'basis': _('Adoption or Fostering'),
+            'weeks': _('If fostering please enter number of weeks:')
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["pitch"].disabled = True
+        self.fields["basis"].disabled = True
+        self.fields["weeks"].disabled = True
