1fb06278f4631ecde9e7029d6b599bf6d8aaea4c
evelynfoy <evelyn.foy@hotmail.com>
2022-05-26 16:16:15 +0000

    Rename fields appearing on form

--- home_animals/forms.py
+++ home_animals/forms.py
@@ -1,5 +1,6 @@
 """ Form """
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import Offer
 
 
@@ -9,3 +10,9 @@ class OfferForm(forms.ModelForm):
         """ Meta """
         model = Offer
         fields = ('animal', 'pitch', 'basis', 'weeks')
+        labels = {
+                    'animal': _('Guest'),
+                    'pitch': _('Why I would make a great owner'),
+                    'basis': _('Adoption or Fostering'),
+                    'weeks': _('If fostering please enter number of weeks:')
+                }
