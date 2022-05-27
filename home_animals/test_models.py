d22051813b1e2361a1255aafcdff80ef1c9cf128
evelynfoy <evelyn.foy@hotmail.com>
2022-05-27 12:10:59 +0000

    Update docstrings with double quotes rather tha single

--- home_animals/test_models.py
+++ home_animals/test_models.py
@@ -12,24 +12,24 @@ class TestModels(TestCase):
     """
 
     def test_animal_type_creation(self):
-        '''
+        """
             Tests creation of Animal Type
             First creates a test animal type with a set value of 'Cat'
             Then tests that the str function on the created object returns the
             same value.
-        '''
+        """
         animal_type = AnimalType(code='Cat', description='Cat')
         self.assertEqual(str(animal_type), 'Cat')
 
     def test_animal_creation(self):
-        '''
+        """
             Tests creation of an Animal object.
             First creates a test animal type with a set value of 'Cat'.
             Then creates a test animal with specific values including
             the test animal type and the name of 'Smokey'.
             Then tests that the str function on the created object returns the
             name 'Smokey'.
-        '''
+        """
         animal_type = AnimalType(code='Cat', description='Cat')
         animal = Animal(name='Smokey', slogan='Grey cat', slug='smokey',
                         type=animal_type,
@@ -37,7 +37,7 @@ class TestModels(TestCase):
         self.assertEqual(str(animal), 'Smokey')
 
     def test_offer_creation(self):
-        '''
+        """
             Tests creation of an Offer object
             Creates a test animal type with a set value of 'Cat'.
             Creates a test animal with specific values including
@@ -47,7 +47,7 @@ class TestModels(TestCase):
             the test animal type, animal and user.
             Then tests that the str function on the created object returns the
             name 'Smokey tom'.
-        '''
+        """
         animal_type = AnimalType(code='Cat', description='Cat')
         animal = Animal(name='Smokey', slogan='Grey cat', slug='smokey',
                         type=animal_type,
